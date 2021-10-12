0a4167e019ee9e237f5bbdb54e7b36fbeac2440c
Nany <72382774+Teragi@users.noreply.github.com>
2021-10-12 07:54:53 +0200

    Create exercice3.py

---
+++ exercice3.py
@@ -0,0 +1,12 @@
+#Version Python de l'exercice 3
+
+a = int(input("a : "))
+b = int(input("b : "))
+c = int(intput("c : "))
+
+if a < b < c:
+    print("Le plus grand des trois est {c}")
+elif a > b > c:
+    print("Le plus grand des trois est {a}")
+elif b > a > c:
+    print("Le plus grand des trois est {b}")
